0e04ecc08556e761449b7b57cbf8e708c2c5c5eb
DeonFranklin <deondeefranklin@gmail.com>
2022-01-28 10:37:11 -0800

    Updates

---
+++ precache-manifest.e55c9c855739331644e790d6cb81df90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fb0e675258fe0af876b535aec1e096",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d1e39120289bb3503a26",
+    "url": "/Portfolio/static/css/main.2313f806.chunk.css"
+  },
+  {
+    "revision": "b8d5efe2bf0493d8beeb",
+    "url": "/Portfolio/static/js/2.17823b0f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Portfolio/static/js/2.17823b0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e39120289bb3503a26",
+    "url": "/Portfolio/static/js/main.e179e5af.chunk.js"
+  },
+  {
+    "revision": "c1bd72ddba6e39931324",
+    "url": "/Portfolio/static/js/runtime-main.24e04e53.js"
+  }
+]);
